9f5ad681fb9e1e5da4bee5b28561dca046d54d2b
henry <henryf.gomez@gmail.com>
2018-01-19 16:27:19 -0600

    Agregada base de proyecto

---
+++ src/PhotoGallery/Controllers/PhotoGalleryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoGallery.Controllers
+{
+    public class PhotoGalleryController : Controller
+    {
+        // GET: PhotoGallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Photo()
+        {
+            return View();
+        }
+
+    }
+}
